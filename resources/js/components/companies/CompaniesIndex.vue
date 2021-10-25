f79384b80bf89f4adb075c4c238b8c7112343a6e
Vitaliy <vito.co@ya.ru>
2021-10-26 01:07:59 +0300

    - vue composition api: companies list

--- resources/js/components/companies/CompaniesIndex.vue
+++ resources/js/components/companies/CompaniesIndex.vue
@@ -1,3 +1,39 @@
 <template>
-    Companies
+  <table class="table-auto">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Adress</th>
+        <th>Websit</th>
+      </tr>
+    </thead>
+    <tbody>
+        <template v-for="item in comanies" :key="item.id">
+            <tr>
+                <td>{{ item.name }}</td>
+                <td>{{ item.email }}</td>
+                <td>{{ item.adress }}</td>
+                <td>{{ item.website }}</td>
+            </tr>
+        </template>
+    </tbody>
+  </table>
 </template>
+
+<script>
+import useCompanies from "../../composable/companies";
+import { onMounted } from "vue";
+
+export default {
+  setup() {
+    const { comanies, getCompanies } = useCompanies();
+
+    onMounted(getCompanies);
+
+    return {
+      comanies,
+    };
+  },
+};
+</script>
